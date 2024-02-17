b08f50613cf998568d0dd2cf4d002062b9523bc1
mehran sf <m.saghebi.dev@gmail.com>
2024-02-17 14:19:00 +0330

    [Initialize] the first commit

---
+++ react-client/src/utilities/socketConnection.js
@@ -0,0 +1,9 @@
+import io from "socket.io-client";
+const options = {
+  auth: { token: "23jrtiheriufyqwidsf" },
+};
+const socket = io.connect("http://localhost:8000", options); //our server is at :3000
+socket.on("connect", (data) => {
+  console.log(data);
+});
+export default socket;
